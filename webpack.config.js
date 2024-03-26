90303b19476b3f7f0568243a491a98297591b7ec
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2024-03-26 18:10:41 +0530

    webpack and typescript setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    bgScript: "./src/bgScript.js",
+    injectedScript: "./src/injectedScript.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devtool: "inline-source-map",
+};
